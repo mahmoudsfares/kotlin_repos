cae527fc172df20b0a2aa8aa768c5f789683ba93
MSaeed <mahmoudsaeed1996@gmail.com>
2022-08-29 13:25:34 +0200

    Exception handling according to type

--- app/src/main/java/com/example/kotlinrevision/MainViewModel.kt
+++ app/src/main/java/com/example/kotlinrevision/MainViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModel
 import com.example.kotlinrevision.data.Resource
 import com.example.kotlinrevision.networking.RetrofitInterface
 import dagger.hilt.android.lifecycle.HiltViewModel
+import retrofit2.HttpException
+import java.io.IOException
 import java.lang.Exception
 import javax.inject.Inject
 
@@ -19,8 +21,19 @@ class MainViewModel @Inject constructor(private val repo: RetrofitInterface): Vi
     suspend fun getTitle() {
         _title.postValue(Resource.Loading())
         try{ _title.postValue(Resource.Success(repo.getTitle().title)) }
-        catch (e: Exception){
-            _title.postValue(Resource.Error("Error occurred: ${e.localizedMessage}"))
+        catch (throwable: Throwable){
+            when(throwable){
+                is HttpException -> {
+                    if(throwable.message().isEmpty())
+                        _title.postValue(Resource.Error("Error ${throwable.code()}"))
+                    else
+                        _title.postValue(Resource.Error("Error ${throwable.code()}: ${throwable.message()}"))
+                }
+                is IOException
+                -> _title.postValue(Resource.Error("No internet connection!"))
+                else
+                -> _title.postValue(Resource.Error("Unexpected error occurred"))
+            }
         }
     }
 }
